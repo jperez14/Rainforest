497c5c2bf37cdd749485e6f8f59f7550eafd01f9
Javier Perez <perez.javi.a@gmail.com>
2014-06-11 16:26:23 -0400

    added some styling and now pulling crystaleis commit with paperclip

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#home'
   resources :reviews
 
   resources :products do 
     resources :reviews
   end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
