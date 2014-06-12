3b061d17359523398e195980c57c6f482dbe64fa
Crystalei Daniels <crystalei2007@gmail.com>
2014-06-12 18:14:09 -0400

    formatted show and edit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
 	def home
 		@products = Product.all.order("created_at DESC")
 		@newproduct = Product.new
+		@review = Review.new
+		# @product = Product.find(params[:id])
 	end
 end
